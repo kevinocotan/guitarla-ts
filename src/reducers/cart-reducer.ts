95d6094be36042028319318c452114bdd6f0128d
kevinocotan <kaocotan@gmail.com>
2024-12-18 17:31:07 -0600

    defininiendo el state inicial

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -1,9 +1,19 @@
-import { Guitar } from "../types"
+import { db } from "../data/db"
+import { CartItem, Guitar } from "../types"
 
-export type CartActions = 
-{type : 'add-to-cart', payload : {item : Guitar}} |
-{type : 'remove-from-cart', payload : {item : Guitar['id']}} |
-{type : 'decrease-quantity', payload : {item : Guitar['id']}} |
-{type : 'increase-quantity', payload : {item : Guitar['id']}} |
-{type : 'clear-cart'} 
+export type CartActions =
+    { type: 'add-to-cart', payload: { item: Guitar } } |
+    { type: 'remove-from-cart', payload: { item: Guitar['id'] } } |
+    { type: 'decrease-quantity', payload: { item: Guitar['id'] } } |
+    { type: 'increase-quantity', payload: { item: Guitar['id'] } } |
+    { type: 'clear-cart' }
 
+export type CarState = {
+    data: Guitar[]
+    cart: CartItem[]
+}
+
+export const initialState: CarState = {
+    data: db,
+    cart: []
+}
